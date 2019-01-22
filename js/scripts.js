10b2a0cda4c44564d8508b554c7d6a3dc4ce18ee
Jimmy Zebroski and Theary Im <jimmy+theary@>
2019-01-22 09:40:28 -0800

    Add Favorite to single array.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var favoriteThing1 = $("input#f1").val();
+    var favoriteThing2 = $("input#f2").val();
+    var favoriteThing3 = $("input#f3").val();
+    var favoriteThing4 = $("input#f4").val();
+    var favoriteThing5 = $("input#f5").val();
+    var favoriteThing6 = $("input#f6").val();
+    var x;
+    var favoriteArray = [];
+
+    favoriteArray.push(favoriteThing1);
+    favoriteArray.push(favoriteThing2);
+    favoriteArray.push(favoriteThing3);
+    favoriteArray.push(favoriteThing4);
+    favoriteArray.push(favoriteThing5);
+    favoriteArray.push(favoriteThing6);
+ 
+  for(x=0;x<favoriteArray.length;x++){
+    $("ul#favoriteList").append("<li>"+favoriteArray[x]+"</li>");
+  }
+
+  event.preventDefault();
+
+    });
+  });
